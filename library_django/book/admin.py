4090d98620fe510fa626cbcf41875d3eb5ce33a9
Natchnene <kondratsiuk.marina@gmail.com>
2023-08-25 12:31:09 +0300

    create author, book, issue book models

--- library_django/book/admin.py
+++ library_django/book/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Book, IssueBook
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(IssueBook)
--- library_django/book/models.py
+++ library_django/book/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from reader.mixins import DateTimeMixin
 
-# Create your models here.
+
+class Author(models.Model, DateTimeMixin):
+    first_name = models.CharField(max_length=100, blank=False)
+    middle_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return f"{self.pk} - {self.first_name} {self.middle_name} {self.last_name}"
+
+
+class Book(models.Model, DateTimeMixin):
+    unique_number = models.CharField(_("unique book number"), max_length=250, blank=False)
+    name = models.CharField(_("book title"), max_length=250, blank=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.pk} - {self.unique_number} {self.name}"
+
+
+class IssueBook(models.Model, DateTimeMixin):
+    book_get = models.DateField(blank=True, null=True)
+    book_return = models.DateField(blank=True, null=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    reader = models.ForeignKey("reader.Reader", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.pk} - {self.book} {self.reader}"
